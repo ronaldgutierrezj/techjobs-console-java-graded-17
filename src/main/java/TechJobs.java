967bd9463e60f0f3dbc4d863cf14a971c66beea8
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2023-09-03 00:48:46 -0500

    printjobs() blank space fixed at the end of the loop

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -127,12 +127,12 @@ public class TechJobs {
            System.out.println("No Results");
        } else {
            for( int i = 0; i < someJobs.size(); i++){
-               System.out.println("*****");
+               System.out.println("\n*****");
 
                for (Map.Entry<String, String> job : someJobs.get(i).entrySet()){
                    System.out.println(job.getKey() + ": " + job.getValue());
                }
-               System.out.println("*****\n");
+               System.out.println("*****");
            }
        }
 
